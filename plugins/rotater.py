4a9d2261cfe16545f8bc66e8431580da83b0fdf1
Nadiya2121 <misshadiya21@gmail.com>
2026-09-06 21:19:20 +0600

    Update rotater.py

--- plugins/rotater.py
+++ plugins/rotater.py
@@ -1,16 +1,16 @@
 import random
 
-def setup(app, tg_app, db, config):
+def setup(app, bot, db, config):
 
     @app.get("/api/get-ad-link")
     async def get_ad_link():
         links = await db.direct_links.find({}).to_list(length=100)
         
-        # কোনো লিংক না থাকলে ব্যাকআপ লিংক
+        # কোনো লিংক না থাকলে ব্যাকআপ গুগল
         if not links:
             return {"url": "https://google.com"}
 
-        # স্মার্ট রোটেশন: র‍্যান্ডমলি একেক সময় একেকটা লিংক সিলেক্ট হবে
+        # স্মার্ট রোটেশন: র‍্যান্ডমলি একেক সময় একেকটি লিংক যাবে
         selected = random.choice(links)
         await db.direct_links.update_one({"_id": selected["_id"]}, {"$inc": {"clicks": 1}})
         
